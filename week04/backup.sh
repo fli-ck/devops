686b68e151fc9cf0e7071389ee1138498b767ef9
Bae Hyeon Woo <yoursanstv@gmail.com>
2026-09-23 11:49:31 +0900

    chore: advise log message on backup.sh

--- week04/backup.sh
+++ week04/backup.sh
@@ -30,8 +30,8 @@ ls -1 "$BACKUP_DIR"
 
 # 로그 기록
 echo "$(date '+%Y-%m-%d %H:%M:%S') 백업 완료: $FILENAME" >> "$BACKUP_DIR/backup.log"
-echo "로그 기록 완료"
+echo "[완료] 로그 기록"
 
 # 로그 기록
 echo "$(date '+%Y-%m-%d %H:%M:%S') 백업 완료: $FILENAME" >> "$BACKUP_DIR/backup.log"
-echo "로그 기록 완료"
+echo "[완료] 로그 기록"
